07cd7273ff152eadbb29f4c86dc15acacb8c2e31
Weblizia Co <weblizia@gmail.com>
2023-01-31 00:28:25 +0330

    Adding useState to AddGift and Adding useEffect to EditGift- Adding Json-server to project and adding dbwiishy to project

--- pages/profile/ProfileAddGift.js
+++ pages/profile/ProfileAddGift.js
@@ -14,26 +14,60 @@ import UploadButtons from "../components/general/UploadButtons";
 import ProfileAvatar from "../components/general/ProfileAvatar.js";
 import ProfileAppBar from "../components/profile/ProfileAppBar";
 import DesireSlider from "../components/general/DesireSlider";
+ 
 
 const theme = createTheme();
 
+
 export default function ProfileAddGiftBar() {
+  
+  
+  const[glink,setGlink]=useState('');
+  const[gname,setGname]=useState('');
+  const[gprice,setGprice]=useState('');
+  const[gdesc,setGdesc]=useState('');
+  const[gdesire,setGdesire]=useState('');
+
+
   const handleSubmit = (event) => {
+
     event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get("email"),
-      password: data.get("password"),
-    });
+
+    // This seems a bit oldfashion so we use STATEs
+    //const data = new FormData(event.currentTarget);
+    // setGlink(data.get('g-link'));
+    // setGname(data.get('g-name'));
+    // setGprice(data.get('g-price'));  
+    // setGdesc(data.get('g-desc'));
+    // setGdesire(data.get('g-desire'));     
+
+      const id= Math.floor(Math.random()*10000)
+      const newGift ={ id:id, ...{link:glink,name:gname,price:gprice,description:gdesc,desire:gdesire}}
+
+       console.log(newGift)   
+      addProduct(newGift);
+
+      setGlink('');
+      setGname('');
+      setGprice('');
+      setGdesc('');
   };
 
+  const addProduct = async(product) => {
+    const response= await fetch('http://localhost:8888/gifts',{
+      method: 'POST',
+      headers:{
+        'Content-type':'application/json'
+      },
+      body: JSON.stringify(product)
+    })
+    const responseData = await response.json();
+  }
+     
   return (
     <Container component="main" maxWidth="sm" sx={{ mb: 4, mt: 12 }}>
       <ProfileAppBar></ProfileAppBar>
-      <Paper
-        elevation={0}
-        sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}
-      >
+      <Paper elevation={0} sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}>
         <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
           <Grid container spacing={2}>
             <Grid
@@ -46,6 +80,7 @@ export default function ProfileAddGiftBar() {
             >
               <ProfileAvatar />
             </Grid>
+
             <Grid
               item
               xs={12}
@@ -58,66 +93,77 @@ export default function ProfileAddGiftBar() {
               <UploadButtons />
               <Typography variant="caption">click to Add gift image</Typography>
             </Grid>
-            <Grid item xs={12} sx={{ mt: 0, pt: 0 }}>
-              <TextField
-                fullWidth
-                id="g-link"
-                label="link of the gift (If available online)"
-                name="g-link"
-                autoComplete="g-link"
-                variant="standard"
-                InputProps={{
-                  endAdornment: <Button variant="text">fetch </Button>,
-                }}
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                required
-                fullWidth
-                name="g-name"
-                label="Gift's name"
-                id="g-name"
-                autoComplete="g-name"
-                variant="standard"
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                required
-                fullWidth
-                name="g-price"
-                label="Gift's price"
-                id="g-price"
-                autoComplete="g-price"
-                variant="standard"
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                fullWidth
-                multiline
-                name="g-desc"
-                label="Describe what is it and why you want it"
-                id="g-desc"
-                autoComplete="g-desc"
-                variant="standard"
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <DesireSlider xs={12}></DesireSlider>
-            </Grid>
-            <Grid item xs={12}></Grid>
-            <Grid item>
-              <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                sx={{ mt: 3, mb: 2 }}
-              >
-                Add To List
-              </Button>
-            </Grid>
+
+           
+              <Grid item xs={12} sx={{ mt: 0, pt: 0 }}>
+                <TextField
+                  fullWidth
+                  id="g-link"
+                  label="link of the gift (If available online)"
+                  name="g-link"
+                  autoComplete="g-link"
+                  variant="standard"
+                  InputProps={{
+                    endAdornment: <Button variant="text">fetch </Button>,
+                  }}
+                  onChange={(e) => {setGlink(e.target.value)}}
+                  value={glink}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="g-name"
+                  label="Gift's name"
+                  id="g-name"
+                  autoComplete="g-name"
+                  variant="standard"
+                  onChange={(e) => {setGname(e.target.value)}}
+                  value={gname}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="g-price"
+                  label="Gift's price"
+                  id="g-price"
+                  autoComplete="g-price"
+                  variant="standard"
+                  onChange={(e) => {setGprice(e.target.value)}}
+                  value={gprice}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  fullWidth
+                  multiline
+                  name="g-desc"
+                  label="Describe what is it and why you want it"
+                  id="g-desc"
+                  autoComplete="g-desc"
+                  variant="standard"
+                  onChange={(e) => {setGdesc(e.target.value)}}
+                  value={gdesc}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <DesireSlider xs={12}></DesireSlider>
+              </Grid>
+              <Grid item xs={12}></Grid>
+              <Grid item>
+                <Button
+                  type="submit"
+                  fullWidth
+                  variant="contained"
+                  sx={{ mt: 3, mb: 2 }}
+                >
+                  Add To List
+                </Button>
+              </Grid>
+         
           </Grid>
         </Box>
       </Paper>
--- pages/profile/ProfileEditGift.js
+++ pages/profile/ProfileEditGift.js
@@ -15,26 +15,45 @@ import ProfileAppBar from "../components/profile/ProfileAppBar";
 import DesireSlider from "../components/general/DesireSlider";
 import DeleteIcon from "@mui/icons-material/Delete";
 import SendIcon from "@mui/icons-material/Send";
+import { useEffect } from "react";
+import { useState } from "react";
 
 const theme = createTheme();
 
 export default function ProfileAddGiftBar() {
+
+  const[glink,setGlink]=useState('');
+  const[gname,setGname]=useState('');
+  const[gprice,setGprice]=useState('');
+  const[gdesc,setGdesc]=useState('');
+  const[gdesire,setGdesire]=useState('');
+
+
   const handleSubmit = (event) => {
     event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get("email"),
-      password: data.get("password"),
-    });
   };
+
+  useEffect(() => {
+    const req = async () => {
+      const res = await fetch("http://localhost:8888/gifts");
+      const responsData = await res.json();
+
+      const gift = responsData.find((p) => p.id == 9970);
+      console.log(gift);
+      setGlink(gift['link'])
+      setGname(gift['name'])
+      setGprice(gift['price'])
+      setGdesc(gift['description'])
+     
+    };
+
+    req();
+  }, []);
 
   return (
     <Container component="main" maxWidth="sm" sx={{ mb: 4, mt: 12 }}>
       <ProfileAppBar></ProfileAppBar>
-      <Paper
-       elevation={0}
-        sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}
-      >
+      <Paper elevation={0} sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}>
         <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 5 }}>
           <Grid container spacing={2}>
             <Grid
@@ -70,6 +89,10 @@ export default function ProfileAddGiftBar() {
                 InputProps={{
                   endAdornment: <Button variant="text">fetch </Button>,
                 }}
+                onChange={(e) => {
+                  setGlink(e.target.value);
+                }}
+                value={glink}
               />
             </Grid>
             <Grid item xs={12}>
@@ -81,6 +104,10 @@ export default function ProfileAddGiftBar() {
                 id="g-name"
                 autoComplete="g-name"
                 variant="standard"
+                onChange={(e) => {
+                  setGname(e.target.value);
+                }}
+                value={gname}
               />
             </Grid>
             <Grid item xs={12}>
@@ -92,6 +119,10 @@ export default function ProfileAddGiftBar() {
                 id="g-price"
                 autoComplete="g-price"
                 variant="standard"
+                onChange={(e) => {
+                  setGprice(e.target.value);
+                }}
+                value={gprice}
               />
             </Grid>
             <Grid item xs={12}>
@@ -103,7 +134,14 @@ export default function ProfileAddGiftBar() {
                 id="g-desc"
                 autoComplete="g-desc"
                 variant="standard"
+                onChange={(e) => {
+                  setGdesc(e.target.value);
+                }}
+                value={gdesc}
               />
+            </Grid>
+            <Grid item xs={12}>
+              <DesireSlider xs={12}></DesireSlider>
             </Grid>
             <Grid item xs={12}>
               <DesireSlider xs={12}></DesireSlider>
