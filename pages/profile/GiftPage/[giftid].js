64dee2388d5cda88bac18c0f9cd677ca7718f161
Reza Rezaeipour <hi@rezarezaeipour.com>
2023-04-17 22:37:33 +0430

    Completing GiftPage but there is a problem in loading data in some cases

--- pages/profile/GiftPage/[giftid].js
+++ pages/profile/GiftPage/[giftid].js
@@ -34,7 +34,7 @@ import WorkIcon from "@mui/icons-material/Work";
 import BeachAccessIcon from "@mui/icons-material/BeachAccess";
 import ShareButton from "../../components/profile/ShareButton";
 import { useRouter } from "next/router";
-import { useState, useEffect,useContext } from "react";
+import { useState, useEffect, useContext } from "react";
 import BottomNav from "../../components/profile/BottomNav";
 import ApiContext from "../../../context/ApiContext";
 
@@ -47,173 +47,172 @@ const theme = createTheme({
 });
 
 export default function ProfileEditGift() {
-
   const router = useRouter();
   const { giftid } = router.query;
-  const { giftdetail} = useContext(ApiContext);
+  const { giftdetail } = useContext(ApiContext);
   const [value, setValue] = React.useState("1");
   const [gift, setGift] = React.useState({});
 
-  useEffect( () => {
-    async function DataReceive() {     
-      const response = await giftdetail(giftid,1) ;  
-   //   console.log(response.gift_detail[0])  
-      setGift(response);
-     
-    }  
-    DataReceive();   
-    //  setGlist(response) 
-  }, [giftid,giftdetail]);
+  useEffect(() => {
+    async function DataReceive() {
+      const response = await giftdetail(giftid, 1);
+      if (response.gift_detail[0]) {
+        setGift(response.gift_detail[0]);
+      }
+      console.log(response.gift_detail[0]);
+    }
+    DataReceive();
+    //  setGlist(response)
+  }, [giftid, giftdetail]);
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
-  return (  
-   
-      <Container component="main" maxWidth="sm" sx={{ mb: 12, mt: 10 }}>
-        <ProfileAppBar></ProfileAppBar>
-        <Paper elevation={0} sx={{ my: { xs: 3, md: 6 }, p: { xs: 0, md: 0 } }}>
-          <Link href={"/profile/Profile/1"}>
-            <CardHeader
-              avatar={
-                <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-                  R
-                </Avatar>
-              }
-              action={
-                <IconButton aria-label="settings">
-                  <MoreVertIcon />
-                </IconButton>
-              }
-              title={gift.giftName}
-              subheader="September 14, 2016"
-            />
-          </Link>
-          <Box sx={{ mt: 0 }}>
-            <Grid container spacing={2} sx={{ mb: 3 }}>
+  return (
+    <Container component="main" maxWidth="sm" sx={{ mb: 12, mt: 10 }}>
+      <ProfileAppBar></ProfileAppBar>
+      <Paper elevation={0} sx={{ my: { xs: 3, md: 6 }, p: { xs: 0, md: 0 } }}>
+        <Link href={"/profile/Profile/1"}>
+          <CardHeader
+            avatar={
+              <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+                R
+              </Avatar>
+            }
+            action={
+              <IconButton aria-label="settings">
+                <MoreVertIcon />
+              </IconButton>
+            }
+            title={gift.name}
+            subheader={gift.created_at}
+          />
+        </Link>
+        <Box sx={{ mt: 0 }}>
+          <Grid container spacing={2} sx={{ mb: 3 }}>
+            <Grid
+              item
+              xs={12}
+              container
+              direction="row"
+              justifyContent="center"
+              alignItems="center"
+            >
+              <Box
+                component="img"
+                sx={{
+                  width: "100%",
+                }}
+                alt={gift.name}
+                src={`${gift.giftImageUrl}`}
+              />
+            </Grid>
+
+            <Grid p={4} pt={1} direction="column">
+              <Grid item>
+                <Rating
+                  sx={{ ml: 0, mb: 3 }}
+                  name="size-medium"
+                  max={5}
+                  readOnly={true}
+                  value={parseInt(gift.desire_rate)}
+                />
+              </Grid>
+
               <Grid
                 item
                 xs={12}
                 container
                 direction="row"
-                justifyContent="center"
+                justifyContent="left"
                 alignItems="center"
               >
-                <Box
-                  component="img"
-                  sx={{
-                    width: "100%",
-                  }}
-                  alt={gift.name}
-                  src={`/giftimages/${gift.giftImageUrl}`}
-                />
+                <Typography>
+                  <Box
+                    component="span"
+                    sx={{
+                      fontWeight: "bold",
+                      m: 1,
+                      ml: 0,
+                      textTransform: "uppercase",
+                    }}
+                  >
+                    Product Name:
+                  </Box>{" "}
+                  {gift.giftName}
+                </Typography>
+              </Grid>
+              <Grid
+                item
+                xs={12}
+                container
+                direction="row"
+                justifyContent="left"
+                alignItems="center"
+              >
+                <Typography>
+                  <Box
+                    component="span"
+                    sx={{
+                      fontWeight: "bold",
+                      m: 1,
+                      ml: 0,
+                      textTransform: "uppercase",
+                    }}
+                  >
+                    Product Price:
+                  </Box>{" "}
+                  {gift.giftPrice}
+                </Typography>
+              </Grid>
+              <Grid
+                item
+                xs={12}
+                container
+                direction="row"
+                justifyContent="left"
+                alignItems="center"
+              >
+                <Typography>
+                  <Box
+                    component="span"
+                    sx={{
+                      fontWeight: "bold",
+                      m: 1,
+                      ml: 0,
+                      textTransform: "uppercase",
+                    }}
+                  >
+                    Product link:
+                  </Box>{" "}
+                  <Link href={gift.giftUrl}>{gift.giftUrl}</Link>
+                </Typography>
+              </Grid>
+              <Grid
+                item
+                xs={12}
+                container
+                direction="row"
+                justifyContent="left"
+                alignItems="center"
+              >
+                <Typography>
+                  <Box
+                    component="span"
+                    sx={{
+                      fontWeight: "bold",
+                      m: 1,
+                      ml: 0,
+                      textTransform: "uppercase",
+                    }}
+                  >
+                    Your Description:
+                  </Box>{" "}
+                  {gift.giftDesc}
+                </Typography>
               </Grid>
 
-              <Grid p={4} pt={1} direction="column">
-                <Grid item>
-                  <Rating
-                    sx={{ ml: 0, mb: 3 }}
-                    name="size-medium"
-                    max={5}
-                    readOnly={true}
-                    value={parseInt(gift.desire_rate)}
-                  />
-                </Grid>
-
-                <Grid
-                  item
-                  xs={12}
-                  container
-                  direction="row"
-                  justifyContent="left"
-                  alignItems="center"
-                >
-                  <Typography>
-                    <Box
-                      component="span"
-                      sx={{
-                        fontWeight: "bold",
-                        m: 1,
-                        ml: 0,
-                        textTransform: "uppercase",
-                      }}
-                    >
-                      Product Name:
-                    </Box>{" "}
-                    {gift.giftName}
-                  </Typography>
-                </Grid>
-                <Grid
-                  item
-                  xs={12}
-                  container
-                  direction="row"
-                  justifyContent="left"
-                  alignItems="center"
-                >
-                  <Typography>
-                    <Box
-                      component="span"
-                      sx={{
-                        fontWeight: "bold",
-                        m: 1,
-                        ml: 0,
-                        textTransform: "uppercase",
-                      }}
-                    >
-                      Product Price:
-                    </Box>{" "}
-                    {gift.giftPrice}
-                  </Typography>
-                </Grid>
-                <Grid
-                  item
-                  xs={12}
-                  container
-                  direction="row"
-                  justifyContent="left"
-                  alignItems="center"
-                >
-                  <Typography>
-                    <Box
-                      component="span"
-                      sx={{
-                        fontWeight: "bold",
-                        m: 1,
-                        ml: 0,
-                        textTransform: "uppercase",
-                      }}
-                    >
-                      Product link:
-                    </Box>{" "}
-                    <Link href={gift.giftUrl}>{gift.giftUrl}</Link>
-                  </Typography>
-                </Grid>
-                <Grid
-                  item
-                  xs={12}
-                  container
-                  direction="row"
-                  justifyContent="left"
-                  alignItems="center"
-                >
-                  <Typography>
-                    <Box
-                      component="span"
-                      sx={{
-                        fontWeight: "bold",
-                        m: 1,
-                        ml: 0,
-                        textTransform: "uppercase",
-                      }}
-                    >
-                      Your Description:
-                    </Box>{" "}
-                    {gift.giftDesc}
-                  </Typography>
-                </Grid>
-
-                {/* <Grid item>
+              {/* <Grid item>
                   <Badge
                     color="secondary"
                     badgeContent={0}
@@ -227,12 +226,11 @@ export default function ProfileEditGift() {
                     <VisibilityIcon />
                   </Badge>
                 </Grid> */}
-              </Grid>
             </Grid>
-          </Box>
-        </Paper>
-        <BottomNav sx={{ border:"solid thin red" }} />
-      </Container>
- 
+          </Grid>
+        </Box>
+      </Paper>
+      <BottomNav sx={{ border: "solid thin red" }} />
+    </Container>
   );
 }
